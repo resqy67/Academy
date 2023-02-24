b3e266c7296b7031d80a309a5824a808a6373dd7
Resqy Hedy Kurniawan <62694501+resqy67@users.noreply.github.com>
2023-02-24 18:26:25 +0800

    update lab 2.1
    update directory

--- Lab2.1.sh
+++ Lab2.1.sh
@@ -1,12 +1,13 @@
+mkdir lab2
 for i in {25..180}
 do
-mkdir client$i
+mkdir lab2/client$i
 done
 for x in {236..453}
 do
-mkdir ops$x
+mkdir lab2/ops$x
 done
 for t in {79..125}
 do
-mkdir dev$t
+mkdir lab2/dev$t
 done
